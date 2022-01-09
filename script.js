88663ac5aae63881d7759beb27ceeeb93adad84a
Mathias Rauls <mathiasorauls@gmail.com>
2022-01-08 19:43:25 -0800

    Menu Toggle Update

--- script.js
+++ script.js
@@ -66,3 +66,21 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+
+// Pop Up Boxes
+
+const chtrLink = document.querySelector('#chtr');
+const gitLink = document.querySelector('#git');
+
+function showAlertChtr() {
+    var txt = "Chatrbox Site Is Undergoing Maintenance";
+    alert(txt);
+}
+
+function showAlertGit() {
+    var txt = "Chatrbox Git Is Undergoing Maintenance";
+    alert(txt);
+}
+
+chtrLink.addEventListener('click', showAlertChtr);
+gitLink.addEventListener('click', showAlertGit);
