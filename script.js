1a2bdd3903f8d33d7bc2cef2d79d14622f347c2f
Mathias Rauls <mathiasrauls321@gmail.com>
2023-03-19 20:43:39 -0700

    Site Overhaul

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
 const navLogo = document.querySelector('#navbar-logo')
 
+
+// Loader
+
+var preload = document.createElement('div');
+
+preload.className = "preloader";
+preload.innerHTML = '<div class="b-ico-preloader"></div><div class="spinner"></div>';
+document.body.appendChild(preload);
+
+window.addEventListener('load', function() {
+    // Uncomment to fade preloader after document load
+    preload.className +=  ' fade';
+    setTimeout(function(){
+        preload.style.display = 'none';
+    },6000);
+})
+
+
 // Display Mobile Menu
 
 const moblieMenu = () => {
@@ -42,8 +60,8 @@ const highlightMenu = () => {
         projMenu.classList.remove('highlight');
         return
     }
-    
-    
+
+
 
     if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
         elem.classList.remove('highlight');
@@ -67,6 +85,32 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
+
+// Slow Scroll
+
+let scrollTimeout = null;
+let isScrolling = false;
+
+function smoothScroll() {
+	if (!isScrolling) {
+		isScrolling = true;
+		let scrollPos = window.pageYOffset;
+		window.scrollTo({
+			top: scrollPos,
+			behavior: 'smooth'
+		});
+		scrollTimeout = setTimeout(function() {
+			isScrolling = false;
+		}, 100);
+	}
+}
+
+window.addEventListener('scroll', function() {
+	clearTimeout(scrollTimeout);
+	smoothScroll();
+});
+
+
 // Pop Up Boxes
 
 // const chtrLink = document.querySelector('#chtr');
@@ -84,3 +128,59 @@ navLogo.addEventListener('click', hideMobileMenu);
 
 // chtrLink.addEventListener('click', showAlertChtr);
 // gitLink.addEventListener('click', showAlertGit);
+
+// Resize Skills
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let skillElements = document.getElementsByClassName("skill");
+//     let lastClickedSkill = null;
+
+//     for (let i = 0; i < skillElements.length; i++) {
+//         skillElements[i].addEventListener("click", function () {
+//         // If there is a previously clicked skill, reset its padding
+//         if (lastClickedSkill) {
+//             lastClickedSkill.style.padding = "20px";
+//         }
+
+//         // Set the current clicked skill's padding to 60px
+//         this.style.padding = "60px";
+
+
+//         // Update the last clicked skill
+//         lastClickedSkill = this;
+//         });
+//     }
+// });
+
+// Make Skills Move
+
+document.addEventListener("DOMContentLoaded", function () {
+	const skillElements = document.querySelectorAll(".skill");
+	const moveAwayDistance = 200;
+
+	document.addEventListener("mousemove", function (event) {
+		const mouseX = event.clientX;
+		const mouseY = event.clientY;
+
+		skillElements.forEach((skillElement) => {
+		const skillRect = skillElement.getBoundingClientRect();
+		const skillX = skillRect.left + skillRect.width / 2;
+		const skillY = skillRect.top + skillRect.height / 2;
+
+		const dx = mouseX - skillX;
+		const dy = mouseY - skillY;
+		const distance = Math.sqrt(dx * dx + dy * dy);
+
+		if (distance < moveAwayDistance) {
+			const moveX = (moveAwayDistance - distance) * (dx / distance);
+			const moveY = (moveAwayDistance - distance) * (dy / distance);
+
+			skillElement.style.transform = `translate(${-moveX}px, ${-moveY}px)`;
+		} else {
+			skillElement.style.transform = "translate(0, 0)"; // Update this line
+		}
+		});
+	});
+});
+
+
